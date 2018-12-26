6ecab6eb31b7cb8f0c73a2f980b0055bc47ed3df
flologicshilpa <40564281+flologicshilpa@users.noreply.github.com>
2018-12-26 10:50:07 +0530

    gthtgdfhg

--- bot.js
+++ bot.js
@@ -111,6 +111,7 @@ bot.on("event",function(event) {
 //greeting dialog
 bot.dialog('GreetingDialog',[
     function (session, args, next) {
+        session.send("welcome hi");
         var name=session.message.user.name;
         var id=session.message.user.id;
         var token1 = session.message.user.token;
